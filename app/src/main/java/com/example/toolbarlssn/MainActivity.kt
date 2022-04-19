6157acd7126800a636e0f2851fadd73f27d0ac57
KMV <9117479@gmail.com>
2022-04-19 17:08:36 +0300

    NC Menu, Toolbar Lesson28fin

--- app/src/main/java/com/example/toolbarlssn/MainActivity.kt
+++ app/src/main/java/com/example/toolbarlssn/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +15,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == android.R.id.home) finish()
+        when(item.itemId){
+            android.R.id.home -> finish()
+            R.id.app_search ->{
+                Toast.makeText(this, "Search is cool!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.app_user_account ->{
+                Toast.makeText(this, "User is great", Toast.LENGTH_SHORT).show()
+            }
+            R.id.info ->{
+                Toast.makeText(this, "Info is new oil", Toast.LENGTH_SHORT).show()
+            }
+            R.id.sttng ->{
+                Toast.makeText(this, "Settings are very impotant", Toast.LENGTH_SHORT).show()
+            }
+        }
         return true
     }
 }
