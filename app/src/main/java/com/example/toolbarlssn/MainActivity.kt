0d9533d8ec6ba6aafbb7f3979b3aa3f940b30aa1
KMV <9117479@gmail.com>
2022-04-19 12:57:55 +0300

    NC Menu, Toolbar Lesson28a

--- app/src/main/java/com/example/toolbarlssn/MainActivity.kt
+++ app/src/main/java/com/example/toolbarlssn/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.toolbarlssn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Edited Title"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home) finish()
+        return true
     }
 }
